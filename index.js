c11156c0d35e8c0de18bb1f8900111caa053c87f
Ananna1299 <anannapaul1299@gmail.com>
2026-03-02 10:34:16 +0600

    added

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const port = process.env.PORT || 3000;
 
 
 
+
 //middleware
 app.use(express.json());
 app.use(cors());
@@ -47,7 +48,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    //await client.connect();
 
 
     const dB = client.db("quick_hire_db");
